986d6e991b3927cd806653fa7cd1bb440b52ea04
pashka1837 <63849514+pashka1837@users.noreply.github.com>
2020-05-25 00:57:06 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+$(".open").click(function() {
+  $(".content").css("display", "grid");
+
+});
+$(".close").click(function() {
+  $(".content").css("display", "none");
+});
+$("a").click(function() {
+  $(".content").css("display", "none");
+});
+// $("a").hover(function() {
+//   $(this).css( {"position": "relative", "left": "40px"});
+// });
+
+// media
+// var x = window.matchMedia("(min-width: 386px)");
+// function mqNavigation(x) {
+//   if (x.matches) {
+//     $(".nav").
+//   }
+// }
